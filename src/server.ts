8b3aee232801434a9f91dad5b6b33de9a5f584c5
kuhora <kuhora123@gmail.com>
2024-12-05 15:21:04 -0300

    API finished

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,28 @@ import fastify from "fastify";
 
 const server = fastify({logger: true});
 
+const teams =[
+    {id: 1, name: "McLaren", base: "Woking, United Kingdom"}, 
+    {id: 2, name: "Mercedes", base: "Brackley, United Kingdom"},
+    {id: 3, name: "Ferrari", base: "Maranello, Italy"},
+    {id: 4, name: "Red Bull Racingerdeces", base: "Milton Keynes, United Kingdom"},
+];
+
+const drivers = [
+    {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
+    {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
+    {id: 3, name: "Lando Norris", team: "Mclaren"},
+    {id: 4, name: "George Russell", team: "Mercedes"},
+]
+
 server.get("/teams", async(request, response) => {
     response.type("application/json").code(200);
-
-    return [{id: 1, name: "ferrari"}];
+    return[teams];
 });
 
 server.get ("/drivers", async(request, response) => {
     response.type("application/json").code(200);
-
-    return [{id: 1, name: "Max Verstappen", team: "Red Bull Racing"}];
+    return [drivers];
 });
 
 server.listen({port: 3636}, () => {
