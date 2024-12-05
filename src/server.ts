570b9c1e5ee8a9c4b57411160a2123cc3b0d7883
kuhora <kuhora123@gmail.com>
2024-12-05 15:14:15 -0300

    API

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,18 @@ import fastify from "fastify";
 const server = fastify({logger: true});
 
 server.get("/teams", async(request, response) => {
-    response.type("application/json").code(200)
+    response.type("application/json").code(200);
 
     return [{id: 1, name: "ferrari"}];
+});
+
+server.get ("/drivers", async(request, response) => {
+    response.type("application/json").code(200);
+
+    return [{id: 1, name: "Max Verstappen", team: "Red Bull Racing"}];
 });
 
 server.listen({port: 3636}, () => {
     console.log ("Server init");
 });
+
